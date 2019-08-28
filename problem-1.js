960596afaac9f152433bd872ad3bc5bef9909e37
shuaibcs <alshuaibi7@gmail.com>
2019-08-28 13:24:18 +0200

    Function - Return statements and values are optional

--- problem-1.js
+++ problem-1.js
@@ -5,3 +5,16 @@ nr_problems - nr_problems - 1
 let sentence = "I've got " + nr_problems + " nr_problems, but using variables ain't one!"
  
 console.log(sentence)
+
+// Function - Return statements and values are optional
+function sayHello() {
+    console.log('Hello,')
+}
+function sayWelcome() {
+    console.log('Welcome to the console.')
+    return
+}
+
+// Call the functions
+let result = sayHello() // result === undefined
+result = sayWelcome() // result === undefined
